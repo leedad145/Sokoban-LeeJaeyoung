69ce6c123f785423cf9f4d26ae2903b555b3ae3e
이재영 <76960587+leedad145@users.noreply.github.com>
2025-11-28 14:58:26 +0900

    <feat>: 이동범위 구현 후 주석처리

--- Program.cs
+++ Program.cs
@@ -4,12 +4,7 @@
     {
         static void Main(string[] args)
         {
-            /*
-            색깔 조정
-            제목 수정 
-            커서 숨김
-            */
-
+            // 콘솔 초기화
             Console.ResetColor();
             Console.BackgroundColor = ConsoleColor.DarkCyan;
             Console.ForegroundColor = ConsoleColor.Magenta;
@@ -17,30 +12,43 @@
             Console.CursorVisible = false;
             Console.Clear();
 
+            // 변수 초기화
             char cPlayer = 'P';
-            int playerPosX = 5;
-            int playerPosY = 10;
+            int playerPosX = 13;
+            int playerPosY = 5;
+            /*
+            int maxPosX = 25;
+            int maxPosY = 10;
+            */
             ConsoleKeyInfo cKey;
 
-            while(true) //무한 루프
+            while(true) // 무한 루프
             {
-                //Player를 그림
+                /*
+                //범위 지정 [0 ~ maxPos]
+                playerPosX = playerPosX < 0 ? 0 : playerPosX;
+                playerPosY = playerPosY < 0 ? 0 : playerPosY;
+                playerPosX = playerPosX > maxPosX ? maxPosX : playerPosX;
+                playerPosY = playerPosY > maxPosY ? maxPosY : playerPosY;
+                */
+          
+                // Player를 좌표에 그림
                 Console.Clear();
                 Console.SetCursorPosition(playerPosX, playerPosY);
                 Console.Write(cPlayer);
 
-                //키를 입력받아 Player의 좌표를 변경함
+                // 키를 입력받아 Player의 좌표를 변경함
                 cKey = Console.ReadKey();
                 switch (cKey.Key)
                 {
                     case ConsoleKey.LeftArrow:
                         playerPosX--;
                         break;
-                    case ConsoleKey.UpArrow:
-                        playerPosY--;
-                        break;
                     case ConsoleKey.RightArrow:
                         playerPosX++;
+                        break;
+                    case ConsoleKey.UpArrow:
+                        playerPosY--;
                         break;
                     case ConsoleKey.DownArrow:
                         playerPosY++;
