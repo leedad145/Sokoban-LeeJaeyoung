f56c35c3e070d1f3c9915132930b66d0272e67f2
이재영 <76960587+leedad145@users.noreply.github.com>
2025-11-28 10:49:00 +0900

    <feat> Console 초기화 기능 구현

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Sokoban
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            색깔 조정
+            제목 수정 
+            커서 숨김
+            */
+
+            Console.ResetColor();
+            Console.BackgroundColor = ConsoleColor.DarkCyan;
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.Title = "My Sokoban";
+            Console.CursorVisible = false;
+            Console.Clear();
+
+
+            Console.Write("입력시 종료: ");
+            Console.Read();
+        }
+    }
+}
